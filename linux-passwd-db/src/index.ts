68e2034dfd864dd1419a117046ce5279d22e72fe
Marko Harjula <marko.harjula@avanio.com>
2023-12-19 08:44:14 +0200

    v0.1.0

---
+++ linux-passwd-db/src/index.ts
@@ -0,0 +1,123 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {AbstractLinuxPasswd, IErrorLike, isValidLine, parsePasswdLine, PasswordEntry, ServiceStatusObject} from '@avanio/os-api-shared';
+import {access, copyFile, execFilePromise, ILinuxSudoOptions} from '@avanio/os-api-linux-utils';
+import {ILoggerLike} from '@avanio/logger-like';
+
+type LinuxHostsDbProps = {
+	/**
+	 * NSS database file path
+	 *
+	 * @default '/var/lib/misc/hosts.db' as for Debian/Ubuntu based systems.
+	 */
+	file?: string;
+	/**
+	 * Makedb command path
+	 * @default '/usr/bin/makedb'
+	 */
+	makedb?: string;
+	/**
+	 * Create a backup of the database file before writing
+	 */
+	backup?: boolean;
+	/**
+	 * Backup file path
+	 *
+	 * @default '/var/lib/misc/hosts.db.bak'
+	 */
+	backupFile?: string;
+};
+
+const initialProps: Required<LinuxHostsDbProps> & ILinuxSudoOptions = {
+	backup: false,
+	backupFile: '/var/lib/misc/hosts.db.bak',
+	file: '/var/lib/misc/hosts.db',
+	makedb: '/usr/bin/makedb',
+	sudo: false,
+};
+
+export class LinuxPasswdDb extends AbstractLinuxPasswd {
+	public readonly name = 'LinuxPasswdDb';
+	private logger?: ILoggerLike;
+	public props: Required<LinuxHostsDbProps> & ILinuxSudoOptions;
+	constructor(props: LinuxHostsDbProps & ILinuxSudoOptions, logger?: ILoggerLike) {
+		super();
+		this.props = {...initialProps, ...props};
+		this.logger = logger;
+	}
+
+	public async status(): Promise<ServiceStatusObject> {
+		const errors: IErrorLike[] = [];
+		try {
+			// check if we can access the file and have write access
+			await access(this.props.file, fs.constants.W_OK, this.props);
+		} catch (e) {
+			errors.push({name: 'FileError', message: `no db file ${this.props.file} found or write access denied`});
+		}
+		try {
+			// check if we can access the makedb executable and have execute access
+			await access(this.props.makedb, fs.constants.X_OK, this.props);
+		} catch (e) {
+			errors.push({name: 'FileError', message: `no makedb executable found from ${this.props.makedb} or access denied`});
+		}
+		if (errors.length > 0) {
+			return {
+				status: 'error',
+				errors,
+			};
+		} else {
+			return {
+				status: 'online',
+			};
+		}
+	}
+
+	protected toOutput(value: PasswordEntry): string {
+		return `${value.username}:${value.password}:${value.uid}:${value.gid}:${value.gecos}:${value.home}:${value.shell}`;
+	}
+
+	protected fromOutput(value: string): PasswordEntry | undefined {
+		if (isValidLine(value)) {
+			return parsePasswdLine(value, this.logger);
+		}
+		return undefined;
+	}
+
+	protected async verifyWrite(value: PasswordEntry): Promise<boolean> {
+		const line = this.toOutput(value);
+		return (await this.listRaw()).some((v) => v === line);
+	}
+
+	protected async storeOutput(value: string[]): Promise<void> {
+		if (this.props.backup) {
+			this.logger?.debug('LinuxHostsDb::backup', this.props.backupFile);
+			await copyFile(this.props.file, this.props.backupFile, undefined, this.props);
+		}
+		const {cmd, args} = this.buildExecParams(['--quiet', '-o', path.resolve(this.props.file), '-']);
+		this.logger?.debug('LinuxHostsDb::storeOutput:', cmd, args);
+		await execFilePromise(cmd, args, Buffer.from(value.join('\n')));
+	}
+
+	protected async loadOutput(): Promise<string[]> {
+		const {cmd, args} = this.buildExecParams(['--quiet', '-u', path.resolve(this.props.file)]);
+		this.logger?.debug('LinuxHostsDb::loadOutput:', cmd, args);
+		const data = await execFilePromise(cmd, args);
+		return data
+			.toString()
+			.split('\n')
+			.map((l) => l.trim())
+			.filter((l) => l.length > 0);
+	}
+
+	private buildExecParams(args: string[]): {cmd: string; args: string[]} {
+		const newArgs = [this.props.makedb, ...args];
+		if (this.props.sudo) {
+			newArgs.unshift('sudo', '-b'); // add sudo and sudo background mode
+		}
+		const cmd = newArgs.shift();
+		if (!cmd) {
+			throw new Error('No command found');
+		}
+		return {cmd, args: newArgs};
+	}
+}
---
+++ linux-passwd-db/test/passwdDb.test.ts
@@ -0,0 +1,116 @@
+/* eslint-disable sort-keys */
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable import/first */
+process.env.NODE_ENV = 'test';
+import 'mocha';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import {copyFile, unlink} from 'fs/promises';
+import {existsSync} from 'fs';
+import {LinuxPasswdDb} from '../src';
+import {PasswordEntry} from '@avanio/os-api-shared';
+
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);
+
+const testData: PasswordEntry = {
+	username: 'test',
+	password: 'x',
+	uid: 1000,
+	gid: 1000,
+	gecos: 'test user',
+	home: '/home/test',
+	shell: '/bin/bash',
+};
+
+const file = './test/passwd.db';
+const backupFile = './test/passwd.db.bak';
+
+let linuxPasswd: LinuxPasswdDb;
+
+describe('linux hosts db API', () => {
+	before(async function () {
+		await copyFile('./test/files/passwd.test.db', file);
+		linuxPasswd = new LinuxPasswdDb({file, backup: true, backupFile, sudo: true});
+		const status = await linuxPasswd.status();
+		if (status.status === 'error') {
+			console.log(status);
+			this.skip();
+		}
+	});
+	beforeEach(async () => {
+		await copyFile('./test/files/passwd.test.db', file);
+		unlink(backupFile);
+	});
+	it('should list entries', async () => {
+		const data = await linuxPasswd.list();
+		expect(data).to.be.an('array');
+		data.forEach((d) => expect(d).to.have.all.keys(['username', 'password', 'uid', 'gid', 'gecos', 'home', 'shell', 'line']));
+		expect(existsSync(backupFile)).to.be.eq(false); // no write yet
+	});
+	it('should add valid entry', async () => {
+		await linuxPasswd.add(testData);
+		const data = await linuxPasswd.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		expect(testEntry).to.deep.equal({line: testEntry?.line, ...testData});
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should modify entry', async () => {
+		let value = (await linuxPasswd.list()).find((d) => d.username === 'root');
+		if (!value) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await linuxPasswd.replace(value, {...value, home: '/home/root'});
+		value = (await linuxPasswd.list()).find((d) => d.username === 'root');
+		if (!value) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		expect(value.home).to.be.eq('/home/root');
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should fail to add not valid entry to file', async () => {
+		await expect(linuxPasswd.add({...testData, username: 'äöäöä'}, 999)).to.be.eventually.rejectedWith(
+			TypeError,
+			'Invalid passwd entry: "username" contains invalid characters. {"username":"äöäöä","password":"x","uid":1000,"gid":1000,"gecos":"test user","home":"/home/test","shell":"/bin/bash"}}',
+		);
+		expect(existsSync(backupFile)).to.be.eq(false); // no write yet
+	});
+	it('should delete entry from file', async () => {
+		await linuxPasswd.add(testData);
+		const data = await linuxPasswd.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await expect(linuxPasswd.delete(testEntry)).to.be.eventually.eq(true);
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should fail to delete from file if cant find line from correct location', async () => {
+		await linuxPasswd.add(testData);
+		const data = await linuxPasswd.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await linuxPasswd.add({username: 'test2', password: 'x', uid: 1000, gid: 1000, gecos: 'test user', home: '/home/test', shell: '/bin/bash'});
+		await expect(linuxPasswd.delete(testEntry)).to.be.eventually.rejectedWith(Error, 'LinuxPasswdDb: might have been changed since the entry was read');
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should return false if deleting not existing entry', async () => {
+		await linuxPasswd.add(testData);
+		const data = await linuxPasswd.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await expect(
+			linuxPasswd.delete({username: 'test2', password: 'x', uid: 1000, gid: 1000, gecos: 'test user', home: '/home/test', shell: '/bin/bash', line: 999}),
+		).to.be.eventually.eq(false);
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	after(async () => {
+		await unlink(file);
+		await unlink(backupFile);
+	});
+});
