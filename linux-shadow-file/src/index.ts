d59d5b92c320171ccd4b4ca24b2991dc4097845c
Marko Harjula <marko.harjula@avanio.com>
2023-12-30 07:31:01 +0200

    initial linux shadow file

---
+++ linux-shadow-file/src/index.ts
@@ -0,0 +1,125 @@
+import * as fs from 'fs';
+import {AbstractLinuxShadow, IErrorLike, isValidLine, parseShadowLine, ServiceStatusObject, ShadowEntry, ShadowFileEntry} from '@avanio/os-api-shared';
+import {access, copyFile, ILinuxSudoOptions, readFile, unlink, writeFile} from '@avanio/os-api-linux-utils';
+import {ILoggerLike} from '@avanio/logger-like';
+
+function eStr(value: string | number | undefined): string {
+	return value === undefined ? '' : value.toString();
+}
+
+type LinuxPasswdProps = {
+	/** file path, defaults to /etc/passwd */
+	file?: string;
+	/** backup file before writing, defaults to false */
+	backup?: boolean;
+	/** backup file path, defaults to /etc/passwd.bak */
+	backupFile?: string;
+};
+
+const initialProps = {
+	backup: false,
+	backupFile: '/etc/shadow.bak',
+	file: '/etc/shadow',
+	sudo: false,
+} satisfies Required<LinuxPasswdProps> & ILinuxSudoOptions;
+
+export class LinuxShadow extends AbstractLinuxShadow {
+	public readonly name = 'LinuxShadowFile';
+	public props: Required<LinuxPasswdProps> & ILinuxSudoOptions;
+	private logger?: ILoggerLike;
+
+	constructor(props: LinuxPasswdProps & ILinuxSudoOptions, logger?: ILoggerLike) {
+		super();
+		this.props = {...initialProps, ...props};
+		this.logger = logger;
+	}
+
+	/**
+	 * Add new entry, overriden to handle backup
+	 * @param value - PasswordEntry
+	 * @param index - optional index to insert entry at
+	 * @returns promise that resolves to true if write was successful
+	 */
+	public override async add(value: ShadowEntry, index?: number): Promise<boolean> {
+		return this.handleRestore(super.add(value, index));
+	}
+
+	/**
+	 * Replace current entry with new one, overriden to handle backup
+	 * @param current - PasswordFileEntry
+	 * @param replace - PasswordEntry
+	 * @returns promise that resolves to true if write was successful
+	 */
+	public override async replace(current: ShadowFileEntry, replace: ShadowEntry): Promise<boolean> {
+		return this.handleRestore(super.replace(current, replace));
+	}
+
+	public async status(): Promise<ServiceStatusObject> {
+		const errors: IErrorLike[] = [];
+		try {
+			// check if we can access the file and have write access
+			await access(this.props.file, fs.constants.W_OK, this.props);
+		} catch (e) {
+			errors.push({name: 'FileError', message: `no hosts file ${this.props.file} found or write access denied`});
+		}
+		if (errors.length > 0) {
+			return {
+				status: 'error',
+				errors,
+			};
+		} else {
+			return {
+				status: 'online',
+			};
+		}
+	}
+
+	protected toOutput(value: ShadowEntry): string {
+		// linux shadow line
+		const data = `${value.username}:${value.password}:${value.changed}:${value.min}:${value.max}:${value.warn}:${eStr(value.inactive)}:${eStr(
+			value.expire,
+		)}:${eStr(value.reserved)}`;
+		if (parseShadowLine(data) === undefined) {
+			throw new Error(`Invalid output line: ${data}`);
+		}
+		return data;
+	}
+
+	protected fromOutput(value: string): ShadowEntry | undefined {
+		if (isValidLine(value)) {
+			return parseShadowLine(value, this.logger);
+		}
+		return undefined;
+	}
+
+	protected async storeOutput(value: string[]): Promise<void> {
+		if (this.props.backup) {
+			await copyFile(this.props.file, this.props.backupFile, undefined, this.props);
+		}
+		return writeFile(this.props.file, Buffer.from(value.join('\n')), this.props);
+	}
+
+	protected async loadOutput(): Promise<string[]> {
+		return (await readFile(this.props.file, this.props)).toString().split('\n');
+	}
+
+	protected async verifyWrite(value: ShadowEntry): Promise<boolean> {
+		const currentLine = this.toOutput(value);
+		return (await this.listRaw()).some((line) => line === currentLine);
+	}
+
+	/**
+	 * if write fails and backup is enabled, restores backup file
+	 * @param isWriteOk - promise that resolves to true if write was successful
+	 * @returns promise that resolves to true if write was successful
+	 */
+	private async handleRestore(isWriteOk: Promise<boolean>): Promise<boolean> {
+		const status = await isWriteOk;
+		if (!status && this.props.backup) {
+			this.logger?.warn(`Write failed, restoring backup file ${this.props.backupFile}`);
+			await copyFile(this.props.backupFile, this.props.file, undefined, this.props);
+			await unlink(this.props.backupFile, this.props);
+		}
+		return status;
+	}
+}
---
+++ linux-shadow-file/test/shadowFile.test.ts
@@ -0,0 +1,123 @@
+/* eslint-disable sort-keys */
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable import/first */
+process.env.NODE_ENV = 'test';
+import 'mocha';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import {copyFile, unlink} from 'fs/promises';
+import {existsSync} from 'fs';
+import {LinuxShadow} from '../src';
+import {ShadowEntry} from '@avanio/os-api-shared';
+
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);
+
+const testData: ShadowEntry = {
+	username: 'test',
+	password: 'x',
+	changed: 16193,
+	min: 0,
+	max: 99999,
+	warn: 7,
+	inactive: undefined,
+	expire: undefined,
+	reserved: undefined,
+};
+
+const file = './test/shadow';
+const backupFile = './test/shadow.bak';
+
+let linuxShadow: LinuxShadow;
+
+describe('linux shadow file API', () => {
+	before(() => {
+		linuxShadow = new LinuxShadow({file, backupFile, backup: true});
+	});
+	beforeEach(async () => {
+		await copyFile('./test/shadow.test', file);
+		unlink(backupFile);
+	});
+	it('should list entries', async () => {
+		const data = await linuxShadow.list();
+		expect(data).to.be.an('array');
+		data.forEach((d) => expect(d).to.have.all.keys('username', 'password', 'changed', 'min', 'max', 'warn', 'inactive', 'expire', 'reserved', 'line'));
+		expect(existsSync(backupFile)).to.be.eq(false); // no write yet
+	});
+	it('should add valid entry to EOF', async () => {
+		await linuxShadow.add(testData);
+		const data = await linuxShadow.list();
+		const testEntry = data[data.length - 1];
+		expect(data[data.length - 1]).to.deep.equal({line: testEntry?.line, ...testData});
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should add valid entry middle of file', async () => {
+		await linuxShadow.add(testData, 4);
+		const data = await linuxShadow.list();
+		expect(data.find((d) => d.line === 4)).to.deep.equal({line: 4, ...testData});
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should modify entry', async () => {
+		let value = (await linuxShadow.list()).find((d) => d.username === 'root');
+		if (!value) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await linuxShadow.replace(value, {...value, changed: 16194});
+		value = (await linuxShadow.list()).find((d) => d.username === 'root');
+		if (!value) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		expect(value.changed).to.be.eq(16194);
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should add valid entry to EOF if index is bigger than array length', async () => {
+		await linuxShadow.add(testData, 999);
+		const data = await linuxShadow.list();
+		const testEntry = data[data.length - 1];
+		expect(data[data.length - 1]).to.deep.equal({line: testEntry?.line, ...testData});
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should fail to add not valid entry to file', async () => {
+		await expect(linuxShadow.add({...testData, username: 'äöäöä'}, 999)).to.be.eventually.rejectedWith(
+			TypeError,
+			'Invalid shadow entry: "username" Invalid. {"username":"äöäöä","password":"x","changed":16193,"min":0,"max":99999,"warn":7}',
+		);
+		expect(existsSync(backupFile)).to.be.eq(false); // no write yet
+	});
+	it('should delete entry from file', async () => {
+		await linuxShadow.add(testData);
+		const data = await linuxShadow.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await expect(linuxShadow.delete(testEntry)).to.be.eventually.eq(true);
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should fail to delete from file if cant find line from correct location', async () => {
+		await linuxShadow.add(testData);
+		const data = await linuxShadow.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await linuxShadow.add({username: 'test2', password: 'x', changed: 16193, min: 0, max: 9999, warn: 7}, 4);
+		await expect(linuxShadow.delete(testEntry)).to.be.eventually.rejectedWith(Error, 'LinuxShadowFile: might have been changed since the entry was read');
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	it('should return false if deleting not existing entry', async () => {
+		await linuxShadow.add(testData);
+		const data = await linuxShadow.list();
+		const testEntry = data.find((d) => d.username === testData.username);
+		if (!testEntry) {
+			throw new Error('Fatal: Test entry not found');
+		}
+		await expect(linuxShadow.delete({username: 'test2', password: 'x', changed: 16193, min: 0, max: 9999, warn: 7, line: 999})).to.be.eventually.eq(false);
+		expect(existsSync(backupFile)).to.be.eq(true);
+	});
+	after(async () => {
+		await unlink(file);
+		await unlink(backupFile);
+	});
+});
