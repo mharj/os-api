409491eeff2a14d1fc9b3c715bfcaa75c37952ec
Marko Harjula <marko.harjula@avanio.com>
2024-07-05 13:34:19 +0300

    add backup permissions

--- shared/src/interfaces/IFileBackupProps.ts
+++ shared/src/interfaces/IFileBackupProps.ts
@@ -1,8 +1,16 @@
+export type LinuxBackupPermission = {
+	posixMode: number;
+};
+
+export type BackupPermission = LinuxBackupPermission;
+
 export type IFileBackupEnabledProps = {
 	/** Create a backup of the database file before writing */
 	backup: true;
 	/** Backup file path */
 	backupFile: string;
+	/** Optional backup permissions */
+	backupPermissions?: BackupPermission;
 };
 
 export type IFileBackupDisabledProps = {
@@ -10,6 +18,8 @@ export type IFileBackupDisabledProps = {
 	backup: false;
 	/** Backup file path */
 	backupFile?: string;
+	/** Optional backup permissions */
+	backupPermissions?: BackupPermission;
 };
 
 export type IFileBackupProps = IFileBackupEnabledProps | IFileBackupDisabledProps;
