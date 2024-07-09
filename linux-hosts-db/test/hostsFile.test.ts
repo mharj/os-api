c5f593b788576cc14e8f2b1d2998d1eab85a846d
Marko Harjula <marko.harjula@avanio.com>
2024-07-09 08:33:03 +0000

    fix sudo on unit testing

--- linux-hosts-db/test/hostsFile.test.ts
+++ linux-hosts-db/test/hostsFile.test.ts
@@ -27,12 +27,22 @@ function modeAsOctal(octal: bigint): bigint {
 	return octal & 0o777n;
 }
 
+let haveSudo = false;
+
 describe('linux hosts db API', () => {
 	before(async function () {
-		await copyFile('./test/files/hosts.test.db', './test/hosts.db', undefined, {sudo: true});
+		// check if sudo command is available
+		try {
+			haveSudo = await test('/usr/bin/sudo', constants.F_OK);
+			haveSudo = true;
+		} catch (e) {
+			haveSudo = false;
+		}
+
+		await copyFile('./test/files/hosts.test.db', './test/hosts.db', undefined, {sudo: haveSudo});
 		linuxHosts = new LinuxHostsDb({
 			file: './test/hosts.db',
-			sudo: true,
+			sudo: haveSudo,
 			backup: true,
 			backupFile: './test/hosts.db.bak',
 			backupPermissions: {posixMode: 0o600},
@@ -44,17 +54,17 @@ describe('linux hosts db API', () => {
 		}
 	});
 	beforeEach(async () => {
-		await copyFile('./test/files/hosts.test.db', './test/hosts.db', undefined, {sudo: true});
-		await chmod('./test/hosts.db', 0o644, {sudo: true});
-		if (await test('./test/hosts.db.bak', constants.F_OK, {sudo: true})) {
-			await unlink('./test/hosts.db.bak', {sudo: true});
+		await copyFile('./test/files/hosts.test.db', './test/hosts.db', undefined, {sudo: haveSudo});
+		await chmod('./test/hosts.db', 0o644, {sudo: haveSudo});
+		if (await test('./test/hosts.db.bak', constants.F_OK, {sudo: haveSudo})) {
+			await unlink('./test/hosts.db.bak', {sudo: haveSudo});
 		}
 	});
 	it('should list hosts entries', async () => {
 		const data = await linuxHosts.list();
 		expect(data).to.be.an('array');
 		data.forEach((d) => expect(d).to.have.all.keys(['line', 'address', 'hostname', 'aliases', 'comment']));
-		expect(await test('./test/hosts.db.bak', constants.F_OK, {sudo: true})).to.be.eq(false);
+		expect(await test('./test/hosts.db.bak', constants.F_OK, {sudo: haveSudo})).to.be.eq(false);
 	});
 	it('should add valid entry', async () => {
 		await linuxHosts.add(testData);
@@ -64,7 +74,7 @@ describe('linux hosts db API', () => {
 			throw new Error('Fatal: Test entry not found');
 		}
 		expect(testEntry).to.deep.equal({line: testEntry.line, ...testData, comment: undefined});
-		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: true})).mode)).to.be.eq(0o600n);
+		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: haveSudo})).mode)).to.be.eq(0o600n);
 	});
 	it('should delete host entry from file', async () => {
 		await linuxHosts.add(testData);
@@ -74,7 +84,7 @@ describe('linux hosts db API', () => {
 			throw new Error('Fatal: Test entry not found');
 		}
 		await expect(linuxHosts.delete(testEntry)).to.be.eventually.eq(true);
-		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: true})).mode)).to.be.eq(0o600n);
+		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: haveSudo})).mode)).to.be.eq(0o600n);
 	});
 	it('should fail to delete from file if cant find line from correct location', async () => {
 		await linuxHosts.add(testData);
@@ -85,7 +95,7 @@ describe('linux hosts db API', () => {
 		}
 		await linuxHosts.add({address: '10.10.10.1', hostname: 'some', aliases: []});
 		await expect(linuxHosts.delete(testEntry)).to.be.eventually.rejectedWith(Error, 'LinuxHostsDb: might have been changed since the entry was read');
-		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: true})).mode)).to.be.eq(0o600n);
+		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: haveSudo})).mode)).to.be.eq(0o600n);
 	});
 	it('should return false if deleting not existing entry', async () => {
 		await linuxHosts.add(testData);
@@ -95,10 +105,10 @@ describe('linux hosts db API', () => {
 			throw new Error('Fatal: Test entry not found');
 		}
 		await expect(linuxHosts.delete({line: 9999, address: '10.10.10.10', hostname: 'asd', aliases: []})).to.be.eventually.eq(false);
-		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: true})).mode)).to.be.eq(0o600n);
+		expect(modeAsOctal((await stat('./test/hosts.db.bak', {sudo: haveSudo})).mode)).to.be.eq(0o600n);
 	});
 	after(async () => {
-		await unlink('./test/hosts.db', {sudo: true});
-		await unlink('./test/hosts.db.bak', {sudo: true});
+		await unlink('./test/hosts.db', {sudo: haveSudo});
+		await unlink('./test/hosts.db.bak', {sudo: haveSudo});
 	});
 });
