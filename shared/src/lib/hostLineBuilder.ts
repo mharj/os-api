d1c22d26cba5ea0f963cbd6c1372812214829d9e
Marko Harjula <marko.harjula@avanio.com>
2024-07-12 12:10:21 +0300

    fix hosts aliases

--- shared/src/lib/hostLineBuilder.ts
+++ shared/src/lib/hostLineBuilder.ts
@@ -2,5 +2,8 @@ import {type HostEntry} from '../types';
 
 export function hostLineBuilder(entry: HostEntry): string {
 	const {address, hostname, aliases, comment} = entry;
-	return `${address}\t${hostname} ${aliases.join(' ')}${comment ? ' # ' + comment : ''}`;
+	if (aliases.length > 0) {
+		return `${address}\t${hostname} ${aliases.join(' ')}${comment ? ' # ' + comment : ''}`;
+	}
+	return `${address}\t${hostname}}${comment ? ' # ' + comment : ''}`;
 }
